a9e9e8751baa6fd7c19319edaa5574176b9009ff
nirdaDV <nir.danon@doubleverify.com>
2025-12-04 12:41:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+self.addEventListener('install', () => {
+  console.log('[SW] Installing');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('[SW] Activating');
+  e.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
+  console.log('[SW] Fetch:', event.request.url);
+  
   if (event.request.url.includes('chatgpt.com/test')) {
-    // IMMEDIATELY respond - blocks network before anything else
+    console.log('[SW] INTERCEPTED - blocking request');
     event.respondWith(new Response(''));
     
-    // Then do async work after response is already sent
     const secChUa = event.request.headers.get('sec-ch-ua') || '';
+    console.log('[SW] sec-ch-ua:', secChUa);
+    
     self.clients.matchAll().then(c => {
       if (c[0]) c[0].postMessage(secChUa.includes('ChatGPTBrowser'));
     });
